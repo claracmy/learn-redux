5346fdf8b2cb17563ec34979879ff3e850baa4d6
claracmy <cheung.cmy@gmail.com>
2018-01-25 11:52:33 +0000

    displaying redux state inside components and updating state with reducers

--- client/components/photogrid.js
+++ client/components/photogrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends React.Component {
   render() {
     return(
       <div className="photo-grid">
-      I'm the photo grid
+       {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,7 +5,18 @@
 // 2. a copy of current state
 
 function posts(state = [], action) {
-  console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+     console.log('incrementing likes!');
+     const i = action.index;
+     return [
+       ...state.slice(0, i), //before the one we're updating
+       {...state[i], likes: state[i].likes + 1 },
+       ...state.slice(i + 1) //after the one we're updating
+     ]
+    default:
+      return state;
+  }
   return state;
 }
 
